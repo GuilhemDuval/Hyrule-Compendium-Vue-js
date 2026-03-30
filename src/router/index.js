d6b21ed30319814729d3645c57d9b46f49eac5c2
Guilhem Duval <guilhem.duval@live.fr>
2026-03-30 16:54:28 +0200

    Correct production routing configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import item_gallery from "@/components/item_gallery.vue"
 import item_single from "@/components/item_single.vue"
 
 const routes = [
-    { path: '/', component: item_gallery},
-    { path: '/:path_id', component: item_single},
+  { path: '/', component: item_gallery },
+  { path: '/:path_id', component: item_single },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory('/'),
+  routes
 })
 
-export {router}
+export { router }
